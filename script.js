d7e0e11dff2c1d338beb3cf096279220edacf7e6
draghici05 <alexdraghici190@gmail.com>
2024-12-02 20:20:35 +0200

    added bootstrap button and tried to draw smth

--- script.js
+++ script.js
@@ -1,35 +1,56 @@
 window.onload = function() {
-    
-     //bug when clicking multiple times on the screen another canvas appears 
-    document.addEventListener('click', function() {
 
-        let canvasContainer = document.getElementById('canvas-container');
-        let canvas = document.createElement('canvas');
-        let context = canvas.getContext('2d'); //terbuia sa le dau declare inainte de functie but we ll live and we ll see
+    let canvasContainer = document.getElementById('canvas-container');
+    // let canvas = document.createElement('canvas');
+    // let ctx = canvas.getContext('2d'); //da nush la startGame() de ce nu merge daca ii dau declare aici
+    let button = document.getElementById('start-button');
 
-        canvasContainer.style.display = 'block';
-        canvasContainer.appendChild(canvas);
+    // document.addEventListener('click', function() { //nu mai trb click daca avem BUTTON
 
-        removeItems();
-        document.removeEventListener('click', arguments.callee); //nvm solved :P
-    });
+    //     // let canvasContainer = document.getElementById('canvas-container');
+    //     // let canvas = document.createElement('canvas');
+    //     // let context = canvas.getContext('2d'); //terbuia sa le dau declare inainte de functie but we ll live and we ll see
+
+    //     canvasContainer.style.display = 'block';
+    //     canvasContainer.appendChild(canvas);
+
+    //     removeItems(); //bug when clic   king multiple times on the screen another canvas appears 
+    //     document.removeEventListener('click', arguments.callee); //nvm solved :P
+    // });
 
     function removeItems() {
-        let canvasContainer = document.getElementById('canvas-container'); // la fel ca la primele
-        document.body.innerHTML = '';
-        document.body.appendChild(canvasContainer);
+         // let canvasContainer = document.getElementById('canvas-container'); // la fel ca la primele
+         document.body.innerHTML = '';
+         document.body.appendChild(canvasContainer);
     }
-//DE CE NU MERGE
-    function startGame() {
-        const canvas = document.querySelector('canvas');
-        const ctx = canvas.getContext('2d'); //la fel ca la fel ca la primele
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.font = '30px Arial'; 
-        ctx.fillStyle = 'white';
-        ctx.fillText('Game Started!', 150, 300);
+
+    button.addEventListener('click', function() {
+        removeItems();
+        canvasContainer.style.display = 'block';
+        canvasContainer.appendChild(canvas);
+    });
+
+    
+//DE CE NU MERGE 
+     function startGame() {
+        let canvas = document.querySelector('canvas'); //also de ce merge doar daca ii dau declare asa si nu poate sa ia de la ctx de SUS i hate javascript
+        let ctx = canvas.getContext('2d'); //la fel ca la fel ca la primele
+        let playerWidth = 120;
+        let playerHeight = 10;
+
+        let player = {
+            x: canvas.width/2 - playerWidth/2,
+            y: canvas.height - playerHeight - 10,
+            width: playerWidth,
+            height: playerHeight,
+        }    
+        
+        ctx.fillStyle = 'red';
+        ctx.fillRect(player.x, player.y, player.width, player.height);
     }
 
     document.addEventListener('click', startGame);
+
 }
 
 
