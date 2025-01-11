9a3f1cf8a0d48c1c3de83d2dfa42fe0218bffb32
draghici05 <alexdraghici190@gmail.com>
2025-01-11 13:11:06 +0200

    commit de urgenta

--- script.js
+++ script.js
@@ -2,24 +2,10 @@ window.onload = function() {
 
     let canvasContainer = document.getElementById('canvas-container');
     // let canvas = document.createElement('canvas');
-    // let ctx = canvas.getContext('2d'); //da nush la startGame() de ce nu merge daca ii dau declare aici
+    // let ctx = canvas.getContext('2d'); 
     let button = document.getElementById('start-button');
 
-    // document.addEventListener('click', function() { //nu mai trb click daca avem BUTTON
-
-    //     // let canvasContainer = document.getElementById('canvas-container');
-    //     // let canvas = document.createElement('canvas');
-    //     // let context = canvas.getContext('2d'); //terbuia sa le dau declare inainte de functie but we ll live and we ll see
-
-    //     canvasContainer.style.display = 'block';
-    //     canvasContainer.appendChild(canvas);
-
-    //     removeItems(); //bug when clic   king multiple times on the screen another canvas appears 
-    //     document.removeEventListener('click', arguments.callee); //nvm solved :P
-    // });
-
     function removeItems() {
-         // let canvasContainer = document.getElementById('canvas-container'); // la fel ca la primele
          document.body.innerHTML = '';
          document.body.appendChild(canvasContainer);
     }
@@ -30,27 +16,79 @@ window.onload = function() {
         canvasContainer.appendChild(canvas);
     });
 
-    
-//DE CE NU MERGE 
-     function startGame() {
-        let canvas = document.querySelector('canvas'); //also de ce merge doar daca ii dau declare asa si nu poate sa ia de la ctx de SUS i hate javascript
-        let ctx = canvas.getContext('2d'); //la fel ca la fel ca la primele
-        let playerWidth = 120;
+    function startGame() {
+        let canvas = document.querySelector('canvas'); 
+        let ctx = canvas.getContext('2d'); 
+        let playerWidth = 140;
         let playerHeight = 10;
 
-        let player = {
+        let player = { 
             x: canvas.width/2 - playerWidth/2,
-            y: canvas.height - playerHeight - 10,
+            y: canvas.height - playerHeight - 8,
             width: playerWidth,
-            height: playerHeight,
-        }    
-        
-        ctx.fillStyle = 'red';
+            height: playerHeight
+        }
+
+    ctx.fillStyle = 'white';
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+    
+
+    ctx.fillstyle = 'green'
+    for (let i = 0; i < brickArray.length; i ++) {
+        let brick = brickArray[i];
+        if (!brick.break) {
+            ctx.fillRect(brick.x, brick.y, brick.width, brick.height)
+        }
+    }
+    }
+
+    function createBricks() {
+        brickArray = [];
+        for (let c = 0; c < brickColumns; c++) {
+            for (let r = 0; r < brickRows; r++) {
+                let brick = {
+                    x: brickX + c*brickWidth + c*10,
+                    y: brickY + r*brickHeight + r*10,
+                    width: brickWidth,
+                    height: brickHeight,
+                }
+                brickArray.push(brick);
+            }
+        }
+        brickCount = brickArray.length;
+    }
+    
+
+    function movePlayer(event) { //este generated vezi tu daca e ok sau fa-o sa nu para ca e generated :D
+        let canvas = document.querySelector('canvas');
+        let ctx = canvas.getContext('2d');
+        let playerWidth = 140;
+        let playerHeight = 10;
+
+        let player = { 
+            x: event.clientX - canvas.offsetLeft - playerWidth / 2,
+            y: canvas.height - playerHeight - 8,
+            width: playerWidth,
+            height: playerHeight
+        }
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = 'white';
         ctx.fillRect(player.x, player.y, player.width, player.height);
     }
 
+    canvasContainer.addEventListener('mousemove', movePlayer);
     document.addEventListener('click', startGame);
 
+    function gameOver() { //asta idk nu trebuie neap si nu e bine oricum
+        let canvas = document.querySelector('canvas');
+        let ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.font = '30px Arial';
+        ctx.fillStyle = 'red';
+        ctx.fillText('Game Over', canvas.width/2 - 100, canvas.height/2);
+    }
+    
 }
 
 
