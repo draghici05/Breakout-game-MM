664c01a1599e8923047fe5631db0aabe773bd5f7
Mitrache Patricia-Andreea <mitrachepatricia22@stud.ase.ro>
2025-01-12 10:57:21 +0200

    made it so the canvas is only called once

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 window.onload = function () {
 
-    let canvasContainer = document.getElementById('canvas-container'); //nu stiu  de ce si 
-    // cum faci dar daca dai declare la 
-    //let brickArray = [];
-    //let score = 0;
-    //let lives = 3;
-    //let player = null; globally nu mai merge butonul :D
-
-    // let canvas = document.createElement('canvas');
-    // let ctx = canvas.getContext('2d'); 
+    let canvasContainer = document.getElementById('canvas-container');
+    let canvas = document.querySelector('canvas');
+    let ctx = canvas.getContext('2d');
     let button = document.getElementById('start-button');
     let level = 1;
+    let brickArray = [];
+    let score = 0;
+    let lives = 3;
+    let player = null;
+    let ball = null;
 
     function removeItems() {
         document.body.innerHTML = '';
@@ -21,21 +20,16 @@ window.onload = function () {
         removeItems();
         canvasContainer.style.display = 'block';
         canvasContainer.appendChild(canvas);
+        startGame();
     });
 
 
     function startGame() {
-        let canvas = document.querySelector('canvas');
-        let ctx = canvas.getContext('2d');
-        let playerWidth = 100 / level;
-        let playerHeight = 10;
-
-            let player = { 
-                x: canvas.width/2 - playerWidth/2,
-                y: canvas.height - playerHeight - 8,
-                width: playerWidth,
-                height: playerHeight
-            }
+        // let canvas = document.querySelector('canvas');
+        // let ctx = canvas.getContext('2d');
+        resetGame();
+        initializePaddle({ clientX: canvas.width / 2 });
+        displayBricks();
 
         // ctx.fillStyle = 'white';
         // ctx.fillRect(player.x, player.y, player.width, player.height);
@@ -48,30 +42,45 @@ window.onload = function () {
         //         ctx.fillRect(brick.x, brick.y, brick.width, brick.height)
         //     }
         // }
-        score();
-        displayBricks();
+
+        // function gameOver() { //asta idk nu trebuie neap si nu e bine oricum
+        //     let canvas = document.querySelector('canvas');
+        //     let ctx = canvas.getContext('2d');
+        //     ctx.clearRect(0, 0, canvas.width, canvas.height);
+        //     ctx.font = '30px Arial';
+        //     ctx.fillStyle = 'red';
+        //     ctx.fillText('Game Over', canvas.width/2 - 100, canvas.height/2);
+        // }
+
+        generateScore();
+        canvasContainer.addEventListener('mousemove', initializePaddle);
+        canvasContainer.addEventListener('mousemove', moveBall);
     }
 
-    function score() {
+    function resetGame() {
+        level = 1;
+        brickArray = [];
+        score = 0;
+        lives = 3;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    function generateScore() {
         ctx.font = '15px Arial';
         ctx.fillStyle = 'white';
-        ctx.fillText('Score: ' + score, 10, 20);
-        ctx.fillText('Lives: ' + lives, canvas.width - 100, 20);
+        ctx.fillText('Score: ' + score, 20, 20);
+        ctx.fillText('Lives: ' + lives, canvas.width - 100 + 10, 20);
     }
 
     function displayBricks() {
 
-        let canvas = document.querySelector('canvas');
-        let ctx = canvas.getContext('2d');
-
-        let brickArray = [];
         let brickWidth = 49.5;
         let brickHeight = 10;
         let brickColumn = (canvas.width - 16) / brickWidth;
         let brickRow = 5 * level;
-        let brickCount = 0;
         let brickX = 8;
-        let brickY = 8;
+        let brickY = 8 + 15 + 8;
+        brickArray = [];
 
         let brickColors = ['yellow', 'orange', 'red', 'purple', 'blue'];
 
@@ -87,7 +96,6 @@ window.onload = function () {
                 brickArray.push(brick);
             }
         }
-        brickCount = brickArray.length;
 
         for (let i = 0; i < brickArray.length; i++) {
             let brick = brickArray[i];
@@ -96,46 +104,44 @@ window.onload = function () {
         }
     }
 
-    
-    let previousPlayerX = 500;
-    let previousPlayerY = 500;
 
-    function movePlayer(event) { //este generated vezi tu daca e ok sau fa-o sa nu para ca e generated :D
-        let canvas = document.querySelector('canvas');
-        let ctx = canvas.getContext('2d');
-        let playerWidth = 100 / level;
-        let playerHeight = 10;
+    let previousPaddleX = 500;
+    let previousPaddleY = 500;
 
-        let player = {
-            x: event.clientX - canvas.offsetLeft - playerWidth / 2,
-            y: canvas.height - playerHeight - 8,
-            width: playerWidth,
-            height: playerHeight
+    function initializePaddle(event) {
+        let paddleWidth = 100 / level;
+        let paddleHeight = 10;
+
+        let paddle = {
+            x: event.clientX - canvas.offsetLeft - paddleWidth / 2,
+            y: canvas.height - paddleHeight - 8,
+            width: paddleWidth,
+            height: paddleHeight
         }
 
         // condition to keep the player inside the canvas :D
-        if (player.x + player.width - 8 > canvas.width) {
-                player.x = canvas.width - 8 - player.width;
+        if (paddle.x + paddle.width - 8 > canvas.width) {
+            paddle.x = canvas.width - 8 - paddle.width;
         }
         else
-        if (player.x < 8) {
-            player.x = 8;
+            if (paddle.x < 8) {
+                paddle.x = 8;
             }
 
-        
-        ctx.clearRect(previousPlayerX, previousPlayerY, playerWidth, playerHeight); // clear the player from the canvas
-        ctx.fillStyle = 'white';
-        ctx.fillRect(player.x, player.y, player.width, player.height);
 
-        previousPlayerX = player.x;
-        previousPlayerY = player.y;
+        ctx.clearRect(previousPaddleX, previousPaddleY, paddleWidth, paddleHeight); // clear the player from the canvas
+        ctx.fillStyle = 'white';
+        ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+
+        previousPaddleX = paddle.x;
+        previousPaddleY = paddle.y;
+
     }
 
     let previousBallX = 500;
     let previousBallY = 500;
-    function ball(event){
-        let canvas = document.querySelector('canvas');
-        let ctx = canvas.getContext('2d');
+
+    function moveBall(event) {
         let ballRadius = 8;
         let ball = {
             x: event.clientX - canvas.offsetLeft,
@@ -146,24 +152,12 @@ window.onload = function () {
         ctx.clearRect(previousBallX - ballRadius - 1, previousBallY - ballRadius - 1, ballRadius * 2 + 2, ballRadius * 2 + 2);
         ctx.fillStyle = 'white';
         ctx.beginPath();
-        ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2);
+        ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
         ctx.fill();
 
         previousBallX = ball.x;
         previousBallY = ball.y;
     }
 
-    canvasContainer.addEventListener('mousemove', movePlayer);
-    canvasContainer.addEventListener('mousemove', ball);
-    displayBricks();
-
-    // function gameOver() { //asta idk nu trebuie neap si nu e bine oricum
-    //     let canvas = document.querySelector('canvas');
-    //     let ctx = canvas.getContext('2d');
-    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //     ctx.font = '30px Arial';
-    //     ctx.fillStyle = 'red';
-    //     ctx.fillText('Game Over', canvas.width/2 - 100, canvas.height/2);
-    // }
 
 }
